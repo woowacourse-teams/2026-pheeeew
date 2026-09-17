b8ea8a82c7f61ec4e982ed7b452afc8945f352b3
Yun Ji <20220723@duksung.ac.kr>
2026-09-17 15:50:28 +0900

    build: Sentry SDK 의존성 연결

--- AN/Pheeeew/shared/build.gradle.kts
+++ AN/Pheeeew/shared/build.gradle.kts
@@ -62,6 +62,7 @@ kotlin {
             implementation(libs.ktor.client.darwin)
         }
         commonMain.dependencies {
+            implementation(libs.sentry)
             implementation(libs.posthog)
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
