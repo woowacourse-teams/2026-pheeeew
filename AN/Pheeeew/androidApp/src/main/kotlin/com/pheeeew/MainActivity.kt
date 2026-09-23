265bd44ba445a1898efa4027616125eb062e8003
BaekCCI <bsy8470@naver.com>
2026-09-23 14:59:02 +0900

    refactor: 불필요한 windowBackground 테마 설정 및 Edge-to-Edge 옵션 제거

--- AN/Pheeeew/androidApp/src/main/kotlin/com/pheeeew/MainActivity.kt
+++ AN/Pheeeew/androidApp/src/main/kotlin/com/pheeeew/MainActivity.kt
@@ -1,19 +1,15 @@
 package com.pheeeew
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
-            navigationBarStyle = SystemBarStyle.dark(Color.TRANSPARENT),
-        )
+        enableEdgeToEdge()
+
         setContent {
             App()
         }
