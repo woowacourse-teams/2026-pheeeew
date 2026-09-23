a03808bfcde55bf40c12df37b784ba2b89567913
todays-sun-day <178suna@gmail.com>
2026-09-23 22:16:28 +0900

    feat: 그룹 스탬프 공통 컴포넌트 추가

---
+++ AN/Pheeeew/shared/src/commonMain/kotlin/com/pheeeew/core/designsystem/theme/AppColors.kt
@@ -0,0 +1,11 @@
+package com.pheeeew.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val RankingContent = Color(0xFF15181B)
+    val RankingAccent = Color(0xFF9DE8D0)
+    val RankingSurface = Color(0xFFF3F4F5)
+    val RankingSecondaryContent = Color(0xFF808080)
+    val Background = Color.White
+}
